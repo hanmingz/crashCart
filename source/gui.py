655d8881fce6eccb366e63590a34e8ea2a8ec034
hanmingz <zuhm2009@hotmail.com>
2018-10-13 13:20:11 -0400

    main in GUI.py GUI menu bar is done, ready to add callback functions

--- source/gui.py
+++ source/gui.py
@@ -1,9 +1,25 @@
 import tkinter as tk
+from functions import *
 
 root = tk.Tk()
-root.geometry("460x320")
+root.geometry("480x300")
 root.title("Smart Crash Cart")
 
+menu = tk.Frame(root, width = 480)
 
+btnUse = tk.Button(menu, text = 'USE', width = 16)
+btnUse.pack(side = tk.LEFT)
+btnList = tk.Button(menu, text = 'LIST', width = 16)
+btnList.pack(side = tk.LEFT)
+btnMaintain = tk.Button(menu, text = 'MAINTANANCE', width = 16)
+btnMaintain.pack(side = tk.LEFT)
 
-root.mainloop()
+menu.pack()
+
+def main():
+	root.mainloop()
+
+if(__name__ == "__main__"):
+	ID = 1
+	(cnx, cursor) = connectSQL()
+	main()
